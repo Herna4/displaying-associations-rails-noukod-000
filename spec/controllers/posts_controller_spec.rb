c9018f025967927bc67ab1d3845ac2ab6ece072d
Herna Michel <labellehernamichel@yahoo.fr>
2019-07-09 01:48:35 +0000

    Automatically backed up by Learn

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -8,10 +8,10 @@ RSpec.describe PostsController, type: :controller do
   #   end
   end
 
-  describe "GET #edit" do
-    it "returns HTTP success" do
-      get :edit, id: @post
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #edit" do
+  #   it "returns HTTP success" do
+  #     get :edit, id: @post
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 end
