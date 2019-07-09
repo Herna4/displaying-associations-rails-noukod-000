d880992061b196592fc8fd8a3fe3f36b00a6b336
Herna Michel <labellehernamichel@yahoo.fr>
2019-07-09 01:48:23 +0000

    Automatically backed up by Learn

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  describe "GET #show" do
-    it "returns HTTP success" do
-      get :show, id: @post
-      expect(response).to have_http_status(:success)
-    end
+  # describe "GET #show" do
+  #   it "returns HTTP success" do
+  #     get :show, id: @post
+  #     expect(response).to have_http_status(:success)
+  #   end
   end
 
   describe "GET #edit" do
